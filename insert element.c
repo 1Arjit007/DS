418d0482e8c396bc65e123ce57b9d9cfb4687462
Arjit kesarwani <38940981+1Arjit007@users.noreply.github.com>
2019-01-13 20:10:41 +0530

    first file created

---
+++ insert element.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+   int arr[5],i;
+   for(i=0;i<5;i++)
+     {
+       printf("Enter the value for arr[%d] : ",i);
+       scanf("%d",&arr[i]);
+     }
+     printf("The array elements are:\n");
+     for(i=0;i<5;i++)
+         printf("%d\t",arr[i]);
+      printf("\n");
+ }
