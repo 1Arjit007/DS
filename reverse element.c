392378d564f27fd0dfb87a9b85f133e4ebb78f89
Arjit kesarwani <38940981+1Arjit007@users.noreply.github.com>
2019-01-14 10:11:55 +0530

    new file

---
+++ reverse element.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int i,j,temp,arr[10]={1,2,3,4,5,6,7,8,9,10};
+  for(i=0,j=9;i<j;i++,j--)
+    {
+      temp=arr[i];
+      arr[i]=arr[j];
+      arr[j]=temp;
+     }
+   printf("After reversing, the aaray is: ");
+   for(i=0;i<10;i++)
+         printf("%d ",arr[i]);
+    printf("\n");
+ }
