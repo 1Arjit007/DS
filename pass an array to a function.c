76a939699596240d656f1530f6c6eea67ba6b16c
Arjit kesarwani <38940981+1Arjit007@users.noreply.github.com>
2019-01-15 09:01:32 +0530

    new file added

---
+++ pass an array to a function.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void func(int val[]);
+void main()
+{
+  int i,arr[6]={1,2,3,4,5,6);
+   func(arr);
+   printf("Contents of array are now : ");
+   for(i=0;i<6;i++)
+         printf("%d ",arr[i]);
+     printf("\n");
+ }
+ void func(int val[])
+ {
+   int sum=0,i;
+   for(i=0;i<6;i++)
+   {
+     val[i]=val[i]*val[i];
+     sum+=val[i];
+   }
+   printf("The sum of squares = %d\n",sum);
+  }
