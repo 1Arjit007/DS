1225edf2ebbe921aee51db0622b4c2a97c9cfc1c
Arjit kesarwani <38940981+1Arjit007@users.noreply.github.com>
2019-02-04 09:18:30 +0530

    new file added

---
+++ changes to the array made inside the functionaffect the original array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void func(int a[]);
+main()
+{
+  int i,arr[5] = {3,6,2,7,1};
+   func(arr);
+   printf("Inside main() : ");
+   for(i=0;i<5;i++)
+         printf("%d  ",arr[i]);
+   printf("\n");
+ }
+ void func(int a[])
+ {
+   int i;
+   printf("Inside func() : ");
+    for(i=0;i<5;i++)
+    {
+      a[i]=a[i]+2;
+      printf("%d ",a[i]);
+     }
+     printf("\n");
+  }
