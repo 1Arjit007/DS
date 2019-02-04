4ab8c1466e2b2e74ab067ffadec279a41b98585d
Arjit kesarwani <38940981+1Arjit007@users.noreply.github.com>
2019-02-04 08:37:00 +0530

    call by reference

---
+++ call by reference.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void ref(int *p,int *q);
+main()
+{
+ int a =5;
+ int b=8;
+ printf("Before calling the function,a=%d and b=%d\n",a,b);
+ ref(&a,&b);
+ printf("After calling the function,a=%d and b=%d\n",a,b);
+}
+void ref(int *p,int *q)
+{
+   (*p)++;
+   (*q)++;
+   printf("Inside functio *p = %d,*q = %d\n",*p,*q);
+ }
